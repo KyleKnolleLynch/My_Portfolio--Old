ebf82a0d5c670c6757bd41e524756dc685ceec5b
KyleKnolleLynch <kylelynch104@gmail.com>
2022-07-04 13:34:22 -0500

    add react ql_blog project

--- src/reactData.js
+++ src/reactData.js
@@ -1,29 +1,37 @@
-import img1 from './assets/images/reactProjects/smoothie-mockup.webp'
-import img2 from './assets/images/reactProjects/onlineCart.webp'
-import img3 from './assets/images/reactProjects/tours-mockup.webp'
-import img4 from './assets/images/reactProjects/resort-mockup.webp'
+import img1 from './assets/images/reactProjects/ql_blog.webp'
+import img2 from './assets/images/reactProjects/smoothie-mockup.webp'
+import img3 from './assets/images/reactProjects/onlineCart.webp'
+import img4 from './assets/images/reactProjects/tours-mockup.webp'
+import img5 from './assets/images/reactProjects/resort-mockup.webp'
 
 export const reactData = [
   {
     id: 1,
     img: img1,
+    title: 'QL Blog',
+    desc: '',
+    projectLink: 'https://ql-blog-taupe.vercel.app/',
+    sourceLink: 'https://github.com/KyleKnolleLynch/ql_blog',
+  },
+  {
+    id: 2,
+    img: img2,
     title: 'Smoothie Builder',
     desc: '',
     projectLink: 'https://smoothie-builder.netlify.app',
     sourceLink: 'https://github.com/kyleknollelynch/smoothie_builder',
   },
   {
-    id: 2,
-    img: img2,
+    id: 3,
+    img: img3,
     title: 'Online Cart',
-    desc:
-      'Shopping cart using context API, Sass, Paypal express checkout button, and Font Awesome icons. Context component is class based, all other components are functional. This app allows users to add or remove items from cart, edit or clear cart, and price totals stay matched up accordingly.',
+    desc: '',
     projectLink: 'https://react-online-cart.netlify.app',
     sourceLink: 'https://github.com/KyleKnolleLynch/React-Online-Cart',
   },
   {
-    id: 3,
-    img: img3,
+    id: 4,
+    img: img4,
     title: 'Central Texas Tours',
     desc: '',
     projectLink: 'https://central-texas-tours.netlify.app',
@@ -31,8 +39,8 @@ export const reactData = [
   },
 
   {
-    id: 4,
-    img: img4,
+    id: 5,
+    img: img5,
     title: 'Resort Rentals',
     desc: '',
     projectLink: 'https://resort-rentals.netlify.app',
